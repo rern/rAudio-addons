26a44085652bfb941f7e8b1dc4a55df892b1ff79
rern <rernrern@gmail.com>
2021-01-11 14:10:51 +0700

    u

--- VU_meter/vu.php
+++ VU_meter/vu.php
@@ -1,20 +1,18 @@
-<?php $time = time();?>
 <html>
 <head>
-<meta charset="UTF-8">
-<style>
-#divvu {
-	width: 230px;
-	height: 230px;
-	background: #000000;
-}
-#vuneedel {
-	margin: -185px 115px;
-	transform: rotate( -28.4deg);
-	transform-origin: bottom;
-	transition-duration: 1500ms;
-}
-</style>
+	<style>
+		#divvu {
+			width: 230px;
+			height: 230px;
+			background: #000000;
+		}
+		#vuneedel {
+			margin: -185px 115px;
+			transform: rotate( -28.5deg);
+			transform-origin: bottom;
+			transition-duration: 1500ms;
+		}
+	</style>
 </head>
 <body>
 <div id="divvu">
@@ -23,29 +21,28 @@
 		<line style="stroke:#000000;" x1="0.5" y1="180" x2="0.5"/>
 	</svg>
 </div>
-<script src="/assets/js/plugin/jquery-2.2.4.min.<?=$time?>.js"></script>
 <script>
 
+var needel = document.getElementById( 'vuneedel' );
 function vu() {
 	var deg = 0;
 	var inc;
-	var $needel = $( '#vuneedel' );
 	vuInt = setInterval( function() {
 		inc = Math.random() * 40
 		deg += inc;
 		if ( deg < -28 ) {
 			deg = -28 + inc;
-		} else if ( deg > 12 ) {
-			deg = 12 - inc;
+		} else if ( deg > 11 ) {
+			deg = 11 - inc;
 		}
-		$needel.css( 'transform', 'rotate( '+ deg +'deg )' );
+		needel.style.transform = 'rotate( '+ deg +'deg )';
 	}, 1000 );
 }
 function vuStop() {
 	clearInterval( vuInt );
-	$( '#vuneedel' ).css( 'transform', 'rotate( -28.4deg )' );
+	needel.style.transform = 'rotate( -28.5deg )';
 }
-vu();
+//vu();
 
 </script>
 </body>
