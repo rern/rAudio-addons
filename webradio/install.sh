b8422783a132022a44d1b74697945e86ba7fe576
rern <rernrern@gmail.com>
2021-11-12 16:36:22 +0700

    Update install.sh

--- webradio/install.sh
+++ webradio/install.sh
@@ -6,7 +6,9 @@ installstart "$1"
 
 curl -sL https://github.com/rern/rAudio-addons/raw/main/webradio/radiofrance.tar.xz | bsdtar xvf - -C /srv/http/data
 chown -R http:http /srv/http/data/webradios*
-count=$( ls -1 /srv/http/data/webradios | wc -l )
+count=$( find $dirdata/webradios -type f \
+			| grep -v '.jpg$\|.gif$' \
+			| wc -l )
 sed -i 's/\("webradio": \).*/\1'$count'/' /srv/http/data/mpd/counts
 
 installfinish
