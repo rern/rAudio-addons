7de5e3325ea3c4495c78d0c7309fab88c59866b9
rern <rernrern@gmail.com>
2022-01-16 11:30:31 +0700

    Create install.sh

---
+++ fix-tft53lcd/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. /srv/http/bash/addons.sh
+
+installstart "$1"
+
+arch=$( uname -m )
+curl -LO https://github.com/rern/rAudio-addons/releases/download/fix-tft35lcd/$arch.tar.xz
+bsdtar --strip 1 -xvf $arch.tar.xz
+
+mv libicu/* /lib
+rm -rf libicu
+pacman -U --noconfirm chromium*
+rm chromium*
+
+sed -i '/#Ignore/ a\Ignore = chromium' /etc/pacman.conf
+
+installfinish
