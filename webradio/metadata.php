eb9c6846c7b3b7f60e5d906cb8ecf2ce9075902f
rern <rernrern@gmail.com>
2022-02-02 11:06:48 +0700

    Update metadata.php

--- webradio/metadata.php
+++ webradio/metadata.php
@@ -1,29 +1,26 @@
 #!/usr/bin/php
 
 <?php
+// usage  : ./metadata.php URL
 
-// usage: ./metadata.php URL
+// source : https://gist.github.com/fracasula/5781710
 
-function getMp3StreamTitle($streamingUrl, $interval, $offset = 0, $headers = true) {
-	if ( ( $headers = get_headers( $streamingUrl ) ) ) {
-		foreach ( $headers as $h ) if ( strpos( strtolower( $h ), 'icy-metaint' ) !== false && ( $interval = explode( ':', $h )[ 1 ] ) ) break;
-	}
-	$context = stream_context_create( [
-		'http' => [
-			'method'     => 'GET',
-			'header'     => 'Icy-MetaData: 1',
-			'user_agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36'
-		]
-	] );
-	if ( $stream = fopen( $streamingUrl, 'r', false, $context ) ) {
-		$buffer = stream_get_contents( $stream, $interval, $offset );
-		fclose( $stream );
-		if ( strpos( $buffer, 'StreamTitle=' ) !== false ) {
-			$title = explode( 'StreamTitle=', $buffer )[ 1 ];
-			echo substr( $title, 1, strpos( $title, ';' ) - 2 );
-		} else {
-			return getMp3StreamTitle( $streamingUrl, $interval, $offset + $interval, false );
-		}
+$url = $argv[ 1 ];
+$context = stream_context_create( [
+	'http' => [
+		'method'     => 'GET',
+		'header'     => 'Icy-MetaData: 1',
+	]
+] );
+$headers = get_headers( $url, false, $context );
+foreach ( $headers as $h ) {
+	if ( strpos( strtolower( $h ), 'icy-metaint' ) !== false ) {
+		$interval = explode( ':', $h )[ 1 ];
+		break;
 	}
 }
-getMp3StreamTitle( $argv[ 1 ], 19200 );
+$stream = fopen( $url, 'r', false, $context );
+$buffer = stream_get_contents( $stream, $interval, $interval );
+fclose( $stream );
+$title = explode( 'StreamTitle=', $buffer )[ 1 ];
+echo substr( $title, 1, strpos( $title, ';' ) - 2 );
