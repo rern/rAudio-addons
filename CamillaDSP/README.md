36f1262712e39fafb989e8c445cd583964f9eb02
rern <rernrern@gmail.com>
2022-03-31 20:40:24 +0700

    u

--- CamillaDSP/README.md
+++ CamillaDSP/README.md
@@ -25,43 +25,7 @@ cargo build --release
 cp target/release/camilladsp /usr/bin
 ```
 
-- Install
-```sh
-getVersion() {
-	user=HEnquist
-	repo=$1
-	version=$( curl -I https://github.com/$user/$repo/releases/latest \
-				| awk -F'/' '/^location/ {print $NF}' \
-				| sed 's/[^v.0-9]//g' )
-}
-
-### binary
-wget https://github.com/rern/rAudio-addons/raw/main/CamillaDSP/camilladsp -P /usr/bin
-chmod +x /usr/bin/camilladsp
-
-### gui
-# python libraries
-su
-cd ..
-getVersion pycamilladsp
-pip install git+https://github.com/HEnquist/pycamilladsp.git@$version
-getVersion pycamilladsp-plo
-pip install git+https://github.com/HEnquist/pycamilladsp-plot.git@v0.6.2
-
-getVersion camillagui-backend
-wget https://github.com/HEnquist/camillagui-backend/releases/download/$version/camillagui.zip
-dir=/srv/http/camillagui
-unzip camillagui -d $dir
-# fixes
-mkdir $dir/coeffs
-sed -i -e 's/configs/config/
-' -e "s|~/camilladsp|$dir|
-" $dir/config/camillagui.yml
-cp $dir/{config/camillagui,active_config}.yml
-cp $dir/{active,default}_config.yml
-```
-
-- On-board audio - `/etc/camilladsp.yml`
+- On-board audio - `camilladsp.yml`
 ```yml
   ...
   playback:
--- CamillaDSP/install.sh
+++ CamillaDSP/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin
 
+alias=cami
+
 getVersion() {
 	user=HEnquist
 	repo=$1
@@ -8,50 +10,11 @@ getVersion() {
 				| sed 's/[^v.0-9]//g' )
 }
 
-pacman -Sy --needed --noconfirm python-aiohttp python-jsonschema python-matplotlib python-pip python-websocket-client
+pacman -Sy --needed --noconfirm python-aiohttp python-jsonschema python-matplotlib python-numpy python-pip python-websocket-client
 
 ### binary
 wget https://github.com/rern/rAudio-addons/raw/main/CamillaDSP/camilladsp -P /usr/bin
 chmod +x /usr/bin/camilladsp
-
-### gui
-# python libraries
-getVersion pycamilladsp
-pip install git+https://github.com/HEnquist/pycamilladsp.git@$version
-getVersion pycamilladsp-plo
-pip install git+https://github.com/HEnquist/pycamilladsp-plot.git@v0.6.2
-
-getVersion camillagui-backend
-wget https://github.com/HEnquist/camillagui-backend/releases/download/$version/camillagui.zip
-dir=/srv/http/camillagui
-unzip camillagui -d $dir
-# fixes
-mkdir $dir/coeffs
-sed -i -e 's/configs/config/
-' -e "s|~/camilladsp|$dir|
-" $dir/config/camillagui.yml
-cp $dir/{config/camillagui,active_config}.yml
-cp $dir/{active,default}_config.yml
-
-cat << EOF > /etc/camilladsp.yml
-devices:
-  samplerate: 44100
-  buffersize: 2048
-  silence_threshold: -60
-  silence_timeout: 3.0
-
-  capture:
-    type: Alsa
-    channels: 2
-    device: hw:Loopback,0
-    format: S32LE
-
-  playback:
-    type: Alsa
-    channels: 2
-    device: hw:0,0
-    format: S16LE
-EOF
 
 cat << EOF > /etc/systemd/system/camilladsp.service
 [Unit]
@@ -62,7 +25,7 @@ StartLimitBurst=10
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/camilladsp /etc/camilladsp.yml
+ExecStart=/usr/bin/camilladsp /srv/http/camillagui/configs/camilladsp.yml
 Restart=always
 RestartSec=1
 StandardOutput=syslog
@@ -77,7 +40,39 @@ CPUSchedulingPriority=10
 WantedBy=graphical.target
 EOF
 
-cat << EOF > /etc/systemd/system/camilladsp.service
+### gui
+# python libraries
+getVersion pycamilladsp
+pip install git+https://github.com/HEnquist/pycamilladsp.git@$version
+getVersion pycamilladsp-plo
+pip install git+https://github.com/HEnquist/pycamilladsp-plot.git@v0.6.2
+
+getVersion camillagui-backend
+wget https://github.com/HEnquist/camillagui-backend/releases/download/$version/camillagui.zip
+dir=/srv/http/camillagui
+unzip camillagui -d $dir
+mkdir $dir/coeffs
+sed -i "s|~/camilladsp|$dir|" $dir/config/camillagui.yml
+
+cat << EOF > /srv/http/camillagui/configs/camilladsp.yml
+devices:
+  samplerate: 44100
+  chunksize: 1024
+
+  capture:
+    type: Alsa
+    channels: 2
+    device: hw:Loopback,0
+    format: S32LE
+
+  playback:
+    type: Alsa
+    channels: 2
+    device: hw:0,0
+    format: S16LE
+EOF
+
+cat << EOF > /etc/systemd/system/camillagui.service
 [Unit]
 Description=CamillaDSP GUI
 After=multi-user.target
@@ -90,5 +85,8 @@ ExecStart=python /srv/http/camillagui/main.py
 [Install]
 WantedBy=multi-user.target
 EOF
+
+ln -s /srv/http/camillagui/configs/camilladsp.yml /srv/http/camillagui/active_config.yml
+chmod -R http:http $dir
 
 systemctl daemon-reload
