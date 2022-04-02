aa86a7d5679c284b50e29d867f9fa8a35410030f
rern <rernrern@gmail.com>
2022-04-02 16:14:32 +0700

    Update install.sh

--- CamillaDSP/install.sh
+++ CamillaDSP/install.sh
@@ -20,16 +20,27 @@ pip install git+https://github.com/HEnquist/pycamilladsp-plot.git@v0.6.2
 
 getVersion camillagui-backend
 wget https://github.com/HEnquist/camillagui-backend/releases/download/$version/camillagui.zip
-dircamillagui=/srv/http/camillagui
+dircamillagui=/srv/http/settings/camillagui
 dircamilladsp=/srv/http/data/camilladsp
 unzip camillagui -d $dircamillagui
 rm camillagui.zip
 
-cp /srv/http/assets/img/icon.png $dircamillagui/build/icon.png
+fileindex=$dircamillagui/build/index.html
 sed -i 's/202020/000000/' $dircamillagui/build/css-variables.css
-sed -i -e 's/favicon.ico/icon.png/
-' -e 's|<noscript>|<div onclick="location.href = 'http://'+ location.hostname" style="position:fixed;right:22px;top:0;font-size:40px;color:#0077b3;font-weight:bold;cursor:pointer;">×</div>&|
-' $dircamillagui/build/index.html
+sed -i 's/<script>/\n&\n/' $fileindex
+sed -i '/<script>/i\
+<div id="close" style="position: fixed;right: 25px;top: 0;font-size: 40px;color: #0077b3;font-weight: bold; cursor: pointer;">×</div>
+' $fileindex
+cat << 'EOF' | sed -i '/<script>/r /dev/stdin' $fileindex
+document.getElementById('close').onclick = function() {
+	var hostname = location.hostname;
+	var http = new XMLHttpRequest();
+	http.open('POST', 'http://'+ hostname +'/cmd.php', true);
+	http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	http.send('cmd=bash&bash=systemctl%20stop%20camillagui');
+	location.href = 'http://'+ hostname;
+}
+EOF
 
 ### binary
 curl -L https://github.com/rern/rAudio-addons/raw/main/CamillaDSP/camilladsp.tar.xz | bsdtar xf - -C /usr/bin
@@ -53,73 +64,82 @@ supported_playback_types: null
 EOF
 
 mkdir -p $dircamilladsp/{coeffs,configs}
-file=$dircamilladsp/configs/camilladsp.yml
-[[ ! -e $file ]] && cat << EOF > $file
+file=$dircamilladsp/configs/default_config..yml
+if [[ ! -e $file ]]; then
+	cat << EOF > $file
 ---
 devices:
-  samplerate: 44100
-  chunksize: 1024
-
+  adjust_period: 10
   capture:
-    type: Alsa
+    avoid_blocking_read: false
     channels: 2
     device: hw:Loopback,0
     format: S32LE
-
-  playback:
+    retry_on_error: false
     type: Alsa
+  capture_samplerate: 0
+  chunksize: 2048
+  enable_rate_adjust: false
+  enable_resampling: false
+  playback:
     channels: 2
     device: hw:0,0
     format: S16LE
-
+    type: Alsa
+  queuelimit: 4
+  rate_measure_interval: 1
+  resampler_type: Synchronous
+  samplerate: 44100
+  silence_threshold: -60
+  silence_timeout: 3
+  stop_on_rate_change: false
+  target_level: 0
 filters:
-  New Filter 1:
+  Volume:
     parameters:
       ramp_time: 200
     type: Volume
-
+mixers: {}
+pipeline:
+- channel: 0
+  names:
+  - Volume
+  type: Filter
+- channel: 1
+  names:
+  - Volume
+  type: Filter
 EOF
-
-ln -sf $file $dircamillagui/active_config.yml
+	cp $dircamilladsp/configs/{default_config,camilladsp}.yml
+fi
 chown -R http:http $dircamillagui
 
 ### service
 cat << EOF > /etc/systemd/system/camillagui.service
 [Unit]
 Description=CamillaDSP GUI
-After=multi-user.target
 
 [Service]
-User=root
 Type=idle
-ExecStart=/usr/bin/python $dircamillagui/main.py
-
-[Install]
-WantedBy=multi-user.target
+ExecStart=/usr/bin/python /srv/http/settings/camillagui/main.py
 EOF
 
 cat << EOF > /etc/systemd/system/camilladsp.service
 [Unit]
 Description=CamillaDSP Daemon
-After=syslog.target
 StartLimitIntervalSec=10
 StartLimitBurst=10
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/camilladsp --port 1234 $dircamilladsp/configs/camilladsp.yml
+ExecStart=/usr/bin/camilladsp --port 1234 /srv/http/data/camilladsp/configs/camilladsp.yml
 Restart=always
 RestartSec=1
 StandardOutput=syslog
 StandardError=syslog
 SyslogIdentifier=camilladsp
-User=root
-Group=root
 CPUSchedulingPolicy=fifo
 CPUSchedulingPriority=10
-
-[Install]
-WantedBy=graphical.target
 EOF
 
 systemctl daemon-reload
