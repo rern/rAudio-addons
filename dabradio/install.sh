865c01487bc0b43b100f5eb130bc596a2d3d04f8
rern <rernrern@gmail.com>
2022-07-04 11:36:22 +0700

    u

---
+++ dabradio/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. /srv/http/bash/addons.sh
+
+installstart "$1"
+
+pacman -Sy --noconfirm dab-scanner
+systemctl enable rtsp-simple-server
+
+cat << 'EOF' > /usr/local/bin/uninstall_dab.sh
+#!/bin/bash
+
+. /srv/http/bash/addons.sh
+
+uninstallstart "$1"
+
+systemctl disable --now rtsp-simple-server
+pacman -R --noconfirm dab-scanner rtsp-simple-server
+
+uninstallfinish
+EOF
+
+installfinish
+
+echo $info Scan DAB radio: Library update
