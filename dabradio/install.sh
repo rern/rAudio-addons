67e3cd2e68bd0fdd3591a5de0ff37478315da3be
rern <rernrern@gmail.com>
2022-09-18 19:04:47 +0700

    Update install.sh

--- dabradio/install.sh
+++ dabradio/install.sh
@@ -4,7 +4,16 @@
 
 installstart "$1"
 
-pacman -Sy --noconfirm dab-scanner
+pacman -Sy --noconfirm dab-scanner inotify-tools
+echo "\
+[Unit]
+Description=DAB Radio metadata
+
+[Service]
+Type=simple
+ExecStart=/srv/http/bash/status-dab.sh
+" > /etc/systemd/system/dab.service
+systemctl daemon-reload
 
 timeout 1 rtl_test -t &> /dev/null && systemctl enable --now rtsp-simple-server
 
