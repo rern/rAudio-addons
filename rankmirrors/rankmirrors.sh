33d9b12a554426be398968e724220ec67bbfb792
rern <rernrern@gmail.com>
2023-05-01 09:02:35 +0700

    u

--- rankmirrors/rankmirrors.sh
+++ rankmirrors/rankmirrors.sh
@@ -2,10 +2,11 @@
 
 alias=rank
 
+# restore 20230501
 #. /srv/http/bash/settings/addons.sh
 
-# 20230317
-[[ -e /srv/http/bash/addons.sh ]] && . /srv/http/bash/addons.sh || . /srv/http/bash/settings/addons.sh
+# 20230501
+[[ -e /srv/http/bash/settings/addons.sh ]] && . /srv/http/bash/settings/addons.sh || . /srv/http/bash/addons.sh
 
 installstart $@
 
